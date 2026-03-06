3ca34a8035cf00ef1a76113ff1f247e53815179e
Mehul-175 <mehul110359@gmail.com>
2026-03-06 11:42:11 +0530

    Initial backend setup with Express and MongoDB

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(express.json()); // Handles application/json
+app.use(express.urlencoded({ extended: true })); // Handles application/x-www-form-urlencoded
+app.use(cookieParser());
+
+
+app.get('/', (req, res)=>{
+    res.send("Api is working...")
+})
+
+export default app;
---
+++ src/server.js
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import app from "./app.js";
+import connectDb from "./config/db.js";
+
+const PORT = process.env.PORT || 6001;
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port: ${PORT}`);
+    connectDb();
+})
